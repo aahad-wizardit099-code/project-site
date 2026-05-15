6d27e981ef332a404507e161a1e86a7337f47818
aahad-wizardit099-code <wizardit099@gmail.com>
2026-05-15 10:03:03 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import Hero from "./components/Hero";
-import Service from "./components/Service";
-import ServiceProvided from "./components/Provide";
-import TradingPlatform from "./components/TradingPlatform";
-import WhyZeroTrade from "./components/WhyZeroTrade";
+import Hero from "./components/Hero.js";
+import Service from "./components/Service.js";
+import ServiceProvided from "./components/Provide.js";
+import TradingPlatform from "./components/TradingPlatform.js";
+import WhyZeroTrade from "./components/WhyZeroTrade.js";
 import Roadmap from "./components/Roadmap";
-import CRMBackOffice from "./components/CRMBack";
-import Infographic from "./components/Infographic";
-import OurServices from "./components/OurServices";
+import CRMBackOffice from "./components/CrmBack.js";
+import Infographic from "./components/Infographic.js";
+import OurServices from "./components/OurServices.js";
 import ContactForm from "./components/ContactForm";
 
 function App() {
